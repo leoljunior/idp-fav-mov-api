2d2295ec77a0b6977a519b17e3221e662f157498
leoljunior <juhhinx@gmail.com>
2022-08-01 12:29:39 -0300

    add

--- src/test/java/com/leoLima/favMovies/controller/CategoryControllerTest.java
+++ src/test/java/com/leoLima/favMovies/controller/CategoryControllerTest.java
@@ -1,11 +1,8 @@
 package com.leoLima.favMovies.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
@@ -15,10 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserters;
 
 import com.leoLima.favMovies.dtos.CategoryDTO;
-import com.leoLima.favMovies.dtos.MovieDTO;
 
 @ActiveProfiles("test")
 @TestMethodOrder(OrderAnnotation.class)
